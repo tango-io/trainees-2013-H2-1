047a65f24cb80c40df8c0c5abf1c0bb5f340a050
Héctor Alejandro Meza Valdez <hmezav@gmail.com>
2013-07-18 14:41:56 -0500

    All files deleted

--- app/assets/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app',[]);
-
-app.config(function($httpProvider){
- angular.forEach(['post', 'put', 'delete'], function(method){
-  $httpProvider.defaults.headers[method] = $httpProvider.defaults.headers[method] || {};
-  $httpProvider.defaults.headers[method]['X-CSRF-Token'] = $('meta[name-csrf-token]').attr('content'); 
- }); 
-});
