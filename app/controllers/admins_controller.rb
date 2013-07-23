74245b62a736807243cdaee4dbe79d83314e0d97
Héctor Alejandro Meza Valdez <hmezav@gmail.com>
2013-07-23 06:09:45 -0500

    admin require to acces to /admins

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,14 +1,23 @@
 class AdminsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :require_admin
 
  def index
-    #@project = Project.all
-    @project = Project.where(user_id: current_user.id)
-  end
+    @project = Project.all
+ end
 
 
   private
   def admin_params
    params.require(:user).permit(:user_name, :user_id)
   end
+ 
+  def require_admin
+    @user = current_user
+    unless current_user.admin?
+      flash[:error] = "You must be an Admin to access this section"
+      redirect_to root_path # halts request cycle
+    end
+  end
+
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    if current_user.admin?
-      render 'admin'
-    else
       render 'user'
-    end
- end
+  end
 end
