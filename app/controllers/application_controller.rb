b385dde662c254acfd36dcb5f8e312da2ea1789b
Oscar Hugo Cardenas Lopez <ohcl87@hotmail.com>
2013-07-24 14:55:57 -0500

    Admin project notifications function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :load_general_vars, if: :user_signed_in? 
+  
   protect_from_forgery with: :exception
+  
+  private
+
+  def load_general_vars
+    @project_count = Project.where(approved: [nil,false]).count
+    @is_admin = User.find(current_user.id).admin
+  end
 end
