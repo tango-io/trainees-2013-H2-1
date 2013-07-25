0f5ee47ad1888dd7685827e24830822c25ac66a3
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-07-25 13:37:19 -0500

    Addec Button to Report Project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -45,7 +45,7 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @owner= User.find(:all, :conditions => "id=#{@project[:user_id]}")
-    #@owner = @user.where(:id => @project.user_id) 
+    @categoryname = Category.find(:all ,:conditions => "id=#{@project[:sub_category_id]}")
   end
 
   private
