2b94465ae8f731afeadc72a561ae6cc895152e7c
Héctor Alejandro Meza Valdez <hmezav@gmail.com>
2013-07-26 10:34:27 -0500

    problems solved

--- config/routes.rb
+++ config/routes.rb
@@ -6,47 +6,4 @@ Star::Application.routes.draw do
   resources :dashboard , :projects, :admins, :categories , :users, :reports
 
   root to: "home#index"
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
->>>>>>> 0caa9afef3e86cb6daac17fdcb55c9fdb880a11d
-  
-  root to: "home#index"
 end
